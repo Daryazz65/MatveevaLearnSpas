2b095b92658f663bda51e69037f67b6f0621dd4b
Дарья <zininadashuta167@gmail.com>
2025-03-01 20:21:42 +0300

    ывс

--- MatveevaLearnSpas/View/Pages/AdminAddPage.xaml.cs
+++ MatveevaLearnSpas/View/Pages/AdminAddPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using MatveevaLearnSpas.AppData;
+﻿using System.Configuration;
+using MatveevaLearnSpas.AppData;
 using MatveevaLearnSpas.Model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Entity.Core.Common.CommandTrees;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
@@ -26,9 +29,12 @@ namespace MatveevaLearnSpas.View.Pages
     public partial class AdminAddPage : Page
     {
         private static MatveevaLearnSpasEntities _context = App.GetContext();
+        private int _selectedUserId;
+        private string connectionString = ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
         public AdminAddPage()
         {
             InitializeComponent();
+            LoadUsers();
             UserDG.ItemsSource = _context.Users.ToList();
             AddRoleCmb.ItemsSource = _context.Roles.ToList();
             AddRoleCmb.DisplayMemberPath = "Name";
@@ -83,7 +89,75 @@ namespace MatveevaLearnSpas.View.Pages
 
         private void EditUserBtn_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (_selectedUserId == 0)
+            {
+                MessageBox.Show("Выберите пользователя для редактирования.");
+                return;
+            }
+
+            string newFullName = txtFullName.Text;
+            string newRole = EditRoleCmb.SelectedItem?.ToString();
+            string newPost = EditPostCmb.SelectedItem?.ToString();
+            string newLogin = txtLogin.Text;
+            string newPassword = txtPassword.Text;
+            DateTime? newRegDate = EditDateRegistrDp.SelectedDate;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE Users SET FullName = @FullName, Role = @Role, Post = @Post, " +
+                               "Login = @Login, Password = @Password, RegistrationDate = @RegDate WHERE Id = @Id";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@FullName", newFullName);
+                    cmd.Parameters.AddWithValue("@Role", newRole);
+                    cmd.Parameters.AddWithValue("@Post", newPost);
+                    cmd.Parameters.AddWithValue("@Login", newLogin);
+                    cmd.Parameters.AddWithValue("@Password", newPassword);
+                    cmd.Parameters.AddWithValue("@RegDate", newRegDate ?? (object)DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Id", _selectedUserId);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Данные обновлены!");
+                        LoadUsers(); // Обновление DataGrid
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка обновления.");
+                    }
+                }
+            }
+        }
+        private void LoadUsers()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Users";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                UserDG.ItemsSource = dt.DefaultView;
+                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+        }
+        private void UserDG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UserDG.SelectedItem is User selectedUser)
+            {
+                txtFullName.Text = selectedUser.FullName;
+                EditRoleCmb.SelectedItem = selectedUser.Role;
+                EditPostCmb.SelectedItem = selectedUser.Post;
+                txtLogin.Text = selectedUser.Login;
+                txtPassword.Text = selectedUser.Password;
+                EditDateRegistrDp.SelectedDate = selectedUser.DateRegistration;
+
+                // Сохраняем ID редактируемого пользователя
+                _selectedUserId = selectedUser.Id;
+            }
         }
     }
 }
